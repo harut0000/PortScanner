26d163a113410a012f931da406325821d3edeed7
Harut Kostanian <arutuh9@gmail.com>
2026-05-20 14:31:12 +0200

    Welcome to Portscanner added.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!111333311");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        Console.WriteLine(@"");
+
+        Console.WriteLine(@"██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗");
+        Console.WriteLine(@"██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝");
+        Console.WriteLine(@"██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  ");
+        Console.WriteLine(@"██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  ");
+        Console.WriteLine(@"╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗");
+        Console.WriteLine(@" ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝");
+
+        Console.WriteLine();
+
+        Console.WriteLine(@"████████╗ ██████╗ ");
+        Console.WriteLine(@"╚══██╔══╝██╔═══██╗");
+        Console.WriteLine(@"   ██║   ██║   ██║");
+        Console.WriteLine(@"   ██║   ██║   ██║");
+        Console.WriteLine(@"   ██║   ╚██████╔╝");
+        Console.WriteLine(@"   ╚═╝    ╚═════╝ ");
+
+        Console.WriteLine();
+
+        Console.WriteLine(@"██████╗  ██████╗ ██████╗ ████████╗███████╗ ██████╗ █████╗ ███╗   ██╗███╗   ██╗███████╗██████╗ ");
+        Console.WriteLine(@"██╔══██╗██╔═══██╗██╔══██╗╚══██╔══╝██╔════╝██╔════╝██╔══██╗████╗  ██║████╗  ██║██╔════╝██╔══██╗");
+        Console.WriteLine(@"██████╔╝██║   ██║██████╔╝   ██║   ███████╗██║     ███████║██╔██╗ ██║██╔██╗ ██║█████╗  ██████╔╝");
+        Console.WriteLine(@"██╔═══╝ ██║   ██║██╔══██╗   ██║   ╚════██║██║     ██╔══██║██║╚██╗██║██║╚██╗██║██╔══╝  ██╔══██╗");
+        Console.WriteLine(@"██║     ╚██████╔╝██║  ██║   ██║   ███████║╚██████╗██║  ██║██║ ╚████║██║ ╚████║███████╗██║  ██║");
+        Console.WriteLine(@"╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝");
+
+        Console.ResetColor();
+
+        Console.ReadKey();
+    }
+}
